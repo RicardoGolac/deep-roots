38ce24dcae12c254c30acde540256cd13bb9d015
Ricardo Golac <rigoba98@hotmail.com>
2019-10-23 13:58:10 -0400

    Hopedully I made client accessible

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import AppNavbar from "./components/AppNavbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <AppNavbar />
+    </div>
+  );
+}
+
+export default App;
