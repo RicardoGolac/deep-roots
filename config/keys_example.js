f93150cc9b258c23c50a7ddbe9ec4f7fcc16f5fe
Ricardo Golac <rigoba98@hotmail.com>
2019-10-22 20:06:13 -0400

    Added example keys.js to protect keys and sensitive information

---
+++ config/keys_example.js
@@ -0,0 +1,8 @@
+//This file holds any configuration variables we may need
+//'keys.js' is ignored by git to protect sensitive information, such as your database's username and password
+//copy this file's contents to another file 'keys.js' and store your MongoLab uri there
+
+module.exports = {
+  //place the URI of your mongo database here.
+  mongoURI: "Insert your mongo Uri here!"
+};
