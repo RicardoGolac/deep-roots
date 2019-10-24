90051435968424ae9b4c9c642e678e9b170fa984
Ricardo Golac <rigoba98@hotmail.com>
2019-10-24 00:03:05 -0400

    Rearanged folders and added some more scripts

---
+++ server/config/keys.js
@@ -0,0 +1,9 @@
+//This file holds any configuration variables we may need
+//'config.js' is ignored by git to protect sensitive information, such as your database's username and password
+//copy this file's contents to another file 'config.js' and store your MongoLab uri there
+
+module.exports = {
+  //place the URI of your mongo database here.
+  mongoURI:
+    "mongodb+srv://guest:guestpassword@cluster0-wbmbd.mongodb.net/test?retryWrites=true&w=majority"
+};
