f86feca76afc8776a564ea68c9cd364fd19ba8ba
Ricardo Golac <rigoba98@hotmail.com>
2019-11-11 12:51:33 -0500

    Added heroku compatibility

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongooseSetup = require("./server/config/database");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const path = require("path");
 // Authentication imports
 const session = require("express-session");
 const MongoStore = require("connect-mongo")(session);
@@ -22,6 +23,16 @@ app.use(bodyParser.json()); // allows us to deal with form data and json data
 
 // Connect to Database
 mongooseSetup.start();
+
+// Serve static assests if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
