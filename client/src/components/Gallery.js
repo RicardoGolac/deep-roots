5c4cf08a5626548aeb0e3eb5586ec1d2fc265216
irenenat <iallman@ufl.edu>
2019-11-12 00:42:44 -0500

    add gallery to vc and add surveyjs-react to project.json

--- client/src/components/Gallery.js
+++ client/src/components/Gallery.js
@@ -1,7 +1,26 @@
 import React, { Component } from "react";
+import "../css/gallery.css";
 
 let viewImage = (imageSrc) => {
-    return <img src={imageSrc} />
+    return (
+        <div class="card mb-3">
+            <div class="row no-gutters">
+                <div class="col-md-8">
+                    <img src={imageSrc} class="card-img" />
+                </div>
+                <div class="col-md-4">
+                    <div class="card-body">
+                        <h5 class="card-title">Title of Art</h5>
+                        <p class="card-text">This is where a description of the piece will be (medium, analysis, etc.)</p>
+                        <p class="card-text"><small class="text-muted">TODO: Date</small></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+
+
+
 }
 
 class Gallery extends Component {
@@ -9,16 +28,16 @@ class Gallery extends Component {
         super(props);
         this.state = {
             pictures: [
-                "https://media.heifer.org/stockphoto-sheep.jpeg?or=0&q=60&crop=faces%2Centropy%2Ccenter&auto=format&fm=jpeg&shad=50&vib=20&duotone-alpha=40&duotone=000000%2C2CFFD4C6&fit=crop&w=367&h=245"
-                , "http://www.thetimes.co.uk/imageserver/image/methode%2Ftimes%2Fprod%2Fweb%2Fbin%2F8bc0ebbc-e918-11e9-b84b-ece3c04125d8.jpg?crop=3349%2C1884%2C117%2C454&resize=685"
-                , "https://cdn.britannica.com/39/95439-050-78B87F34/Adult-sheep-lambs.jpg"
+                "https://thedoris.files.wordpress.com/2011/11/1-dsc01528-2-small.jpg?w=1400"
+                , "https://cdn-az.allevents.in/banners/e72dc5d6aef11c31d6aaeb2b3ae950d5-rimg-w720-h540-gmir.jpg"
+                , "https://thedoris.files.wordpress.com/2011/11/zulu-chief.jpg"
             ]
         }
     }
-    render() { 
+    render() {
         return (
-            <div>
-                {this.state.pictures.map(function(picture) {
+            <div id="gallery-container">
+                {this.state.pictures.map(function (picture) {
                     return viewImage(picture);
                 })}
             </div>
