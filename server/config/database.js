958109bee319c97fc63461996e4522854ea6a99e
Ricardo Golac <rigoba98@hotmail.com>
2019-11-12 14:26:20 -0500

    try catch added in database.js

--- server/config/database.js
+++ server/config/database.js
@@ -1,15 +1,31 @@
 const mongoose = require("mongoose");
-const db = require("./keys");
 
-module.exports = {
-  start: function() {
-    mongoose
-      .connect(process.env.MONGODB_URI || db.mongoURI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-      })
-      .then(() => console.log("MongoDB Connected..."))
-      .catch(err => console.log(err));
-  },
-  connection: mongoose.connection
-};
+try {
+  const db = require("./keys.js");
+  // do stuff
+  module.exports = {
+    start: function() {
+      mongoose
+        .connect(db.mongoURI, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true
+        })
+        .then(() => console.log("MongoDB Connected..."))
+        .catch(err => console.log(err));
+    },
+    connection: mongoose.connection
+  };
+} catch (ex) {
+  module.exports = {
+    start: function() {
+      mongoose
+        .connect(process.env.MONGODB_URI, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true
+        })
+        .then(() => console.log("MongoDB Connected..."))
+        .catch(err => console.log(err));
+    },
+    connection: mongoose.connection
+  };
+}
