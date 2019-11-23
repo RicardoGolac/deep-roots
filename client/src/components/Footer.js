774c3716b46e9d51c3e2c94f9f423da7c2a0de67
Ricardo Golac <rigoba98@hotmail.com>
2019-11-23 18:21:54 -0500

    Refactored Footer Component

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,16 +4,18 @@ import { Container, Nav, NavItem } from "reactstrap";
 
 function Footer() {
   return (
-    <Container className="footer-view-container">
-      <Nav justified>
-        <NavItem eventKey={1}>Privacy policy</NavItem>
-        <NavItem eventKey={2} title="Item">
-          Terms & Conditions
-        </NavItem>
-        <NavItem eventKey={3}>Some other professional link</NavItem>
-      </Nav>
-      <div className="text-center small copyright">© Deep Roots</div>
-    </Container>
+    <div className="footer-view-container">
+      <div className="footer-container">
+        <Nav justified>
+          <NavItem eventKey={1}>Privacy policy</NavItem>
+          <NavItem eventKey={2} title="Item">
+            Terms & Conditions
+          </NavItem>
+          <NavItem eventKey={3}>Some other professional link</NavItem>
+        </Nav>
+        <div className="text-center small copyright">© Deep Roots</div>
+      </div>
+    </div>
   );
 }
 
