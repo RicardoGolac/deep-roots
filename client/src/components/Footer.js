90db0f84e06eb1e8b7c21825417d55cdc27da72c
Ricardo Golac <rigoba98@hotmail.com>
2019-11-23 20:15:42 -0500

    Social Media Icons added

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,19 +1,27 @@
 import React from "react";
 import "../css/footer.css";
-import { Container, Nav, NavItem } from "reactstrap";
+import { Nav, NavItem } from "reactstrap";
+import { SocialIcon } from "react-social-icons";
 
 function Footer() {
   return (
     <div className="footer-view-container">
       <div className="footer-container">
         <Nav justified>
-          <NavItem eventKey={1}>Privacy policy</NavItem>
-          <NavItem eventKey={2} title="Item">
-            Terms & Conditions
+          <NavItem eventKey={1}>
+            <SocialIcon url="https://www.youtube.com/" />
           </NavItem>
-          <NavItem eventKey={3}>Some other professional link</NavItem>
+          <NavItem eventKey={2} title="Item">
+            <SocialIcon url="https://www.instagram.com/" />
+          </NavItem>
+          <NavItem eventKey={3}>
+            <SocialIcon url="https://twitter.com/home?lang=en" />
+          </NavItem>
+          <NavItem eventKey={4}>
+            <SocialIcon url="https://www.pinterest.com" />
+          </NavItem>
         </Nav>
-        <div className="text-center small copyright">© Deep Roots</div>
+        <div className="text-center small copyright">© Deep Roots Creative</div>
       </div>
     </div>
   );
